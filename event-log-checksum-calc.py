40ecb5fd6eba8a5f7e8e24ae9061484e44d4fa01
Ron Nefyodov <48227040+Ron-Nefyodov@users.noreply.github.com>
2021-12-30 07:21:59 -0700

    Update event-log-checksum-calc.py

--- event-log-checksum-calc.py
+++ event-log-checksum-calc.py
@@ -9,9 +9,17 @@ def rewrite_size_record(evtx):
         four_bytes = f.read(4)
         while four_bytes:
             four_bytes = f.read(4)
+            if four_bytes == b'\x45\x6c\x66\x43':
+                four_bytes = f.read(4)
+                if four_bytes == b'\x68\x6e\x6b\x00':
+                    f.seek(16,1)
+                    next_record_id = f.read(8)
+                    f.seek(-16,1)
+                f.seek(-4,1)
             if four_bytes == b'\x2a\x2a\x00\x00':
                 offset_first_size = f.tell() - 4
                 size = f.read(4)
+                record_id = f.read(8)
             if four_bytes == size:
                 if f.read(4) == b'\x2a\x2a\x00\x00':
                     f.seek(-4, 1)
@@ -21,6 +29,14 @@ def rewrite_size_record(evtx):
                     f.write(new_size)
                     f.seek(offset_second_size - 4)
                     f.write(new_size)
+                else:
+                    f.seek(-4, 1)
+                    if((struct.unpack('<Q',next_record_id))[0] - 1 == (struct.unpack('<Q',record_id))[0]):
+                        offset_second_size = f.tell()
+                        new_size = struct.pack('<L', (offset_second_size - offset_first_size))
+                        f.seek(offset_second_size - 4)
+                        f.write(new_size)
+
         f.close()
 
 
@@ -34,9 +50,9 @@ def rewrite_records_checksum(evtx_file):
         four_bytes = f.read(4)
         while four_bytes:
             four_bytes = f.read(4)
-            if four_bytes == b'\x45\x6c\x66\x43':
+            if four_bytes == b'\x45\x6c\x66\x46':
                 four_bytes = f.read(4)
-                if four_bytes == b'\x68\x6e\x6b\x00':
+                if four_bytes == b'\x69\x6c\x65\x00':
                     offset_start_chunk = f.tell()-8
                     offset_checksum = f.tell() + 44
                     f.seek(36, 1)
@@ -90,8 +106,42 @@ def rewrite_chunk_header_checksum(evtx):
                     f.seek(- 4, 1)
         f.close()
 
+def rewrite_last_record_offset(evtx):
+    with open(evtx, 'r+b') as f:
+        size = b''
+        four_bytes = f.read(4)
+        while four_bytes:
+            four_bytes = f.read(4)
+            if four_bytes == b'\x45\x6c\x66\x43':
+                four_bytes = f.read(4)
+                if four_bytes == b'\x68\x6e\x6b\x00':
+                    f.seek(-8,1)
+                    offset_start_chunk = f.tell()
+                    f.seek(42,1)
+                    offset_of_offset_last_event_record = f.tell()
+                    f.seek(-34, 1)
+            if four_bytes == b'\x2a\x2a\x00\x00':
+                size = struct.unpack('<L', f.read(4))
+                f.seek(size[0] - 8, 1)
+                if f.read(4) != b'\x2a\x2a\x00\x00':
+                    f.seek(-size[0], 1)
+                    offset_last_record = f.tell()
+                    f.seek(offset_of_offset_last_event_record)
+                    f.write(struct.pack('<L',offset_last_record-offset_start_chunk))
+
+
+
+# def rewrite_first_record_number_and_identifier(evtx):
+# def rewrite_last_record_number_and_identifier(evtx):
+# def rewrite_first_chunk(evtx):
+# def rewrite_last_chunk(evtx):
+# def rewrite_number_chunks(evtx):
+# def rewrite_file_header_checksum(evtx):
+# def rewrite_next_record_identifier(evtx):
+# def rewrite_number_of_chunks(evtx):
 
 if __name__ == '__main__':
-    rewrite_size_record("C:\\Desktop\\security.evtx")
-    rewrite_records_checksum("C:\\Desktop\\security.evtx")
-    rewrite_chunk_header_checksum("C:\\Desktop\\security.evtx")
+    rewrite_size_record("C:\\Users\\U227835\\Desktop\\security.evtx")
+    rewrite_last_record_offset("C:\\Users\\U227835\\Desktop\\security.evtx")
+    rewrite_records_checksum("C:\\Users\\U227835\\Desktop\\security.evtx")
+    rewrite_chunk_header_checksum("C:\\Users\\U227835\\Desktop\\security.evtx")
